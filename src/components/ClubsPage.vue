76ddaf4c0545a0bf8bc1d388d62144fe5cf7ccd7
yourskenny <71455080+yourskenny@users.noreply.github.com>
2023-10-30 00:15:05 +0800

    ()

--- src/components/ClubsPage.vue
+++ src/components/ClubsPage.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="clubs-page">
+    <Navbar />
     我是 clubs
     <!-- Clubs and Organizations Content -->
     <!-- Implement the card layout for clubs and organizations here (e.g., Traditional Martial Arts Association, Flute and Xiao Association, Folk Music Ensemble, Cross Talk Association) -->
@@ -7,7 +8,10 @@
 </template>
 
 <script>
+import Navbar from "./Navbar.vue";
+
 export default {
-  name: 'ClubsPage',
+  name: "ClubsPage",
+  components: { Navbar },
 };
 </script>
